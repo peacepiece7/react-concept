fa2af229cb15d470e69b4554cdf25fdb24ca4d28
taetae <peacepiece7@naver.com>
2023-03-09 19:14:55 +0900

    feat : router -> hashRouter로 변경

--- basic/src/App.jsx
+++ basic/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Route, Routes, Link } from "react-router-dom"
 import Profile from "./components/Profile"
 import Avator from "./components/Avator"
 import TotalCount from "./components/TotalCount"
@@ -17,50 +17,51 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <a href="/">basic</a>
-        <a href="/mouseTracker">mouseTracker</a>
-        <a href="/mento">mento (useState)</a>
-        <a href="/mentors">mentors (useReducer)</a>
-        <a href="/mentorsImmer">mentors (immer)</a>
-        <a href="/theme">theme (useContext)</a>
-        <a href="/perfomance">mentors (performance)</a>
-        <a href="/product">product (loading)</a>
-        <a href="/styledComponent">styledcomponent</a>
-        <a href="/tailwind">tailwind</a>
+        <Link to="/mouseTracker">mouseTracker</Link>
+        <Link to="/">basic</Link>
+        <Link to="mouseTracker">mouseTracker</Link>
+        <Link to="mento">mento (useState)</Link>
+        <Link to="mentors">mentors (useReducer)</Link>
+        <Link to="mentorsImmer">mentors (immer)</Link>
+        <Link to="theme">theme (useContext)</Link>
+        <Link to="perfomance">mentors (performance)</Link>
+        <Link to="product">product (loading)</Link>
+        <Link to="styledComponent">styledcomponent</Link>
+        <Link to="tailwind">tailwind</Link>
       </nav>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Profile title="Product Manager" name="Amanda" isNew={true}>
-                  <Avator imageSrc="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2864&q=80" />
-                </Profile>
-                <Profile title="Software Enginner" name="James">
-                  <Avator imageSrc="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2960&q=80" />
-                </Profile>
-                <Profile title="Cloud Enginner" name="Sara">
-                  <Avator imageSrc="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787" />
-                </Profile>
-                <Profile title="Data Analisist" name="Judi">
-                  <Avator imageSrc="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1940" />
-                </Profile>
-                <TotalCount />
-              </>
-            }
-          ></Route>
-          <Route path="/mouseTracker" element={<AppXY />} />
-          <Route path="/mento" element={<AppMento />} />
-          <Route path="/mentors" element={<AppMentors />} />
-          <Route path="/mentorsImmer" element={<AppMentorsImmer />} />
-          <Route path="/theme" element={<AppTheme />} />
-          <Route path="/perfomance" element={<AppPerformance />} />
-          <Route path="/product" element={<AppProducts />} />
-          <Route path="/styledComponent" element={<AppStyledComponent />} />
-          <Route path="/tailwind" element={<AppTailwind />} />
-        </Routes>
-      </BrowserRouter>
+
+      <Routes>
+        <Route exact path="/mouseTracker" element={<AppXY />} />
+        <Route exact path="/mento" element={<AppMento />} />
+        <Route exact path="/mentors" element={<AppMentors />} />
+        <Route exact path="/mentorsImmer" element={<AppMentorsImmer />} />
+        <Route exact path="/theme" element={<AppTheme />} />
+        <Route exact path="/perfomance" element={<AppPerformance />} />
+        <Route exact path="/product" element={<AppProducts />} />
+        <Route exact path="/styledComponent" element={<AppStyledComponent />} />
+        <Route exact path="/tailwind" element={<AppTailwind />} />
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Profile title="Product Manager" name="Amanda" isNew={true}>
+                <Avator imageSrc="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2864&q=80" />
+              </Profile>
+              <Profile title="Software Enginner" name="James">
+                <Avator imageSrc="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2960&q=80" />
+              </Profile>
+              <Profile title="Cloud Enginner" name="Sara">
+                <Avator imageSrc="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787" />
+              </Profile>
+              <Profile title="Data Analisist" name="Judi">
+                <Avator imageSrc="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1940" />
+              </Profile>
+              <TotalCount />
+            </>
+          }
+        />
+      </Routes>
     </div>
   )
 }
--- basic_ellie/src/AppProducts.jsx
+++ basic_ellie/src/AppProducts.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
-import './App.css';
-import Products from './components/Products';
+import React from "react"
+import { Route, HashRouter, Link, Routes } from "react-router-dom"
+import "./App.css"
 
 export default function AppProducts() {
-  const [showProducts, setShowProducts] = useState(true);
   return (
     <div>
-      {showProducts && <Products />}
-      <button onClick={() => setShowProducts((show) => !show)}>Toggle</button>
+      hi
+      <HashRouter>
+        <Routes>
+          <Route exact href="/hi" element={<div>Hello, world!</div>}></Route>
+          <Route exact href="/" element={<div>FOO!</div>}></Route>
+        </Routes>
+      </HashRouter>
     </div>
-  );
+  )
 }
